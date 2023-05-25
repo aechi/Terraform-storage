20f67fdd06c5010a211388254aa71ece59c2c9d7
Abdelbasset ECHI <Abdelbasset.ECHI@actiatelecom.fr>
2023-05-25 20:27:47 +0200

    Create new project

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  skip_provider_registration = "true"
+  subscription_id = "97c37559-6638-46e2-9ef5-451dd20445e0"
+  tenant_id = "c6537e5c-aa83-4924-886b-877ad74ea895"
+  client_id = "d2313b26-81e7-44aa-ab80-89c87e6f649f"
+  client_secret = "A8T8Q~NjM_FtPeeor1jPMu6e14~qGzzBKwMPkbHj"
+features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "resourcegroup-test-tbd-3"
+  location = "westeurope"
+}
