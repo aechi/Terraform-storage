8a5beadb5a459c58fd351101013293b3d5e895f3
Abdelbasset ECHI <Abdelbasset.ECHI@actiatelecom.fr>
2023-05-26 10:37:21 +0200

    Delete client_secret

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,9 @@
 terraform {
-  backend "azurerm" {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.57.0"
+    }
   }
 }
 
@@ -13,7 +17,33 @@ provider "azurerm" {
 features {}
 }
 
+
+# Create the resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "resourcegroup-test-tbd-3"
-  location = "westeurope"
+  name     = "devops-training-rg"
+  location = "West Europe"
+}
+
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "webapp-asp-assessment"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+# Create the web app, pass in the App Service Plan ID
+resource "azurerm_linux_web_app" "webapp" {
+  name                  = "webapp-java-assessment"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
+  https_only            = true
+  site_config { 
+    always_on               = false
+    application_stack {
+      java_version           = "11"      
+    }
+  }
 }
