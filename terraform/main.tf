ef9b34fef147ddc4fa6af87e57489a89fdade34c
Abdelbasset ECHI <Abdelbasset.ECHI@actiatelecom.fr>
2023-05-26 20:08:35 +0200

    Update client_secret

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
   subscription_id = "97c37559-6638-46e2-9ef5-451dd20445e0"
   tenant_id = "c6537e5c-aa83-4924-886b-877ad74ea895"
   client_id = "d2313b26-81e7-44aa-ab80-89c87e6f649f"
-  client_secret = "XXXX"
+  client_secret = "A8T8Q~NjM_FtPeeor1jPMu6e14~qGzzBKwMPkbHj"
 features {}
 }
 
